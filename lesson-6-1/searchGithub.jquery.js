bf93fe21c28ad2a54f4a51f6feaa60338ffad7f4
kubkowski <jakub.opara@gmail.com>
2014-08-13 14:14:26 +0200

    Finished lesson 27 (Creating plugins) and course

--- lesson-6-1/searchGithub.jquery.js
+++ lesson-6-1/searchGithub.jquery.js
@@ -15,22 +15,62 @@ if (typeof Object.create !== 'function') {
 				self.elem = elem;
 				self.$elem = $(elem);
 				self.url = 'https://api.github.com/users/';
-				if (typeof options === 'string') {
-					self.search = options + '/repos';
-				} else {
-					self.search = options.search + '/repos';
-					self.options = $.extend({}, $.fn.searchGithub.options, options);
-					console.log(self.options);
-				}
+				self.search = (typeof options === 'string')
+					? options
+					: options.search;
+
+				self.options = $.extend({}, $.fn.searchGithub.options, options);
+				self.refresh(1);
 		},
+
+		refresh: function(length){
+			var self = this;
+			
+			setTimeout(function(){
+				self.fetch().done(function(results){
+					results = self.limit(results.data, self.options.limit);
+					self.buildFrag(results);
+					self.display();
+					if (typeof self.options.onComplete === 'function') {
+						self.options.onComplete.apply(self.elem, arguments);
+					};
+
+					if (self.options.refresh) {
+						self.refresh();
+					};
+
+				});
+			}, length || self.options.refresh);
+		},
+
 		fetch: function(){
 			return $.ajax({
-				url: this.url + this.search,
+				url: this.url + this.search + '/repos',
 				dataType: 'jsonp'
 			});
 		},
+		
+		buildFrag: function(results){
+			var self = this;
+			self.repos = $.map(results, function(obj, index) {
+				return $(self.options.wrapEachWith).append(obj.name);
+			});
+		},
+
 		display: function(){
-			this.$elem.html(self.repos);
+			var self = this;
+			
+			if (self.options.transition === 'none' || !self.options.transition) {
+				self.$elem.html(self.repos);
+			} else {
+				self.$elem[self.options.transition](500, function(){
+					$(this).html(self.repos)[self.options.transition](500);
+				});
+			}			
+		},
+
+		limit: function(obj, count){
+			return obj.slice(0, count);
 		}
 
 
@@ -40,10 +80,16 @@ if (typeof Object.create !== 'function') {
 		return this.each(function(){
 			var github = Object.create(Github);
 			github.init(options, this);
+			$.data(this, 'searchGithub', github)
 		});
 	};
 	$.fn.searchGithub.options = {
-		search: 'kubkowski'
+		search: 'kubkowski',
+		wrapEachWith: '<li></li>',
+		limit: 10,
+		refresh: null,
+		onComplete: null,
+		transition: 'fadeToggle'
 	};
 
 })(jQuery, window, document );
